765695039de34f05b6002f78df60a6388da1d59e
Robert Szafarczyk <robertszafa@gmail.com>
2022-01-19 15:57:05 +0000

    Add freq to resutls. Show only kernel BRAM usage.

--- velfg/test_bench.py
+++ velfg/test_bench.py
@@ -22,21 +22,24 @@ SIZES = [
     '300x300'
     ]
 
-
+# Found in report.htlm under area. The numbers seems to be stable, inrespective of kernel source.
+BRAM_STATIC_PARTITION = 492
 
 
 if __name__ == "__main__":
     header = ['kernel'] + SIZES
     time_data = []
     ram_usage_data = []
+    freq_data = []
 
     for kernel_type in KERNEL_TYPES:
         kernel_times = [kernel_type]
         kernel_ram_usage = [kernel_type]
+        kernel_freq = [kernel_type]
 
         for size in SIZES:
-            binary = f'{BIN_PATH}{KERNEL_PREFIX}_{kernel_type}_{size}.fpga'
-            report = f'{BIN_PATH}{KERNEL_PREFIX}_{kernel_type}_{size}.fpga.prj/acl_quartus_report.txt'
+            binary = f'{BIN_PATH}{KERNEL_PREFIX}_{kernel_type}_{size}.fpga_emu'
+            report = f'{BIN_PATH}{KERNEL_PREFIX}_{kernel_type}_{size}.prj/acl_quartus_report.txt'
             print(f'\n--- {binary} ---')
             
             if not os.path.isfile(binary):
@@ -47,12 +50,10 @@ if __name__ == "__main__":
                 for _ in range(REPEAT):
                     out = os.popen(binary).read()
                     time_str = re.findall("\d+\.\d+", out)
-                    print(time_str)
                     try:
                         time = float(time_str[0]) if float(time_str[0]) < time else time
                     except:
-                        print('Something went wrong with getting time from ' + binary + ". Skipping..")
-                        break
+                        print(f'{binary} output\n: {out}')
 
                 kernel_times.append(time)
             
@@ -65,18 +66,25 @@ if __name__ == "__main__":
                     report_str = report_f.read()
 
                 ram_usage_str = re.findall("RAM blocks: (.*?)/", report_str)
+                freq_str = re.findall("Actual clock freq: (\d+)", report_str)
 
                 rams = 0
+                freq = 0
                 try:
-                    rams = re.sub("[^0-9]", "", ram_usage_str[0])
+                    rams = int(re.sub("[^0-9]", "", ram_usage_str[0])) - BRAM_STATIC_PARTITION
                 except:
-                    print('Something went wrong with ram usage from ' + report + ". Skipping..")
+                    print('Something went wrong with  getting ram usage from ' + report + ". Skipping..")
+                try:
+                    freq = freq_str[0]
+                except:
+                    print('Something went wrong with getting freq from ' + report + ". Skipping..")
 
                 kernel_ram_usage.append(rams)
-                print(rams)
+                kernel_freq.append(freq)
         
         time_data.append(kernel_times)
         ram_usage_data.append(kernel_ram_usage)
+        freq_data.append(kernel_freq)
 
 
     with open(RESULTS_FILE, 'w', encoding='UTF8', newline='') as f:
@@ -87,6 +95,11 @@ if __name__ == "__main__":
         writer.writerows(time_data)
 
         writer.writerow([""])
-        writer.writerow(["RAM (out of 2700)"])
+        writer.writerow(["RAM (only kernel system)"])
         writer.writerow(header)
         writer.writerows(ram_usage_data)
+
+        writer.writerow([""])
+        writer.writerow(["Frequency"])
+        writer.writerow(header)
+        writer.writerows(freq_data)
